6e8b05e37054a7a4cabbfdd6417ee9c1daa724b7
AndreLuzzi <andreluzzi@outlook.com.br>
2026-06-10 10:30:03 -0300

    limpeza

--- banco de dados/Agenda.sql
+++
@@ -1,8 +0,0 @@
-create table contatos(
-	id_contatos serial primary key,
-	nome varchar(50),
-	telefone varchar(20),
-	email varchar(50)
-);
-
-select*from contatos;
--- banco de dados/POO.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE curso (
-id SERIAL PRIMARY KEY,
-nome VARCHAR(100) NOT NULL,
-descricao TEXT,
-carga_horaria INTEGER NOT NULL
-);
