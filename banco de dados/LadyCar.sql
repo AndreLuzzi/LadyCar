fcd0c4b32d0b0bdcce5fd122750af7a9a3388478
AndreLuzzi <andreluzzi@outlook.com.br>
2026-06-10 16:23:03 -0300

    banco atualizado

--- banco de dados/LadyCar.sql
+++ banco de dados/LadyCar.sql
@@ -12,9 +12,14 @@ ALTER TABLE cliente ADD COLUMN numero VARCHAR(10);
 ALTER TABLE cliente ADD COLUMN bairro VARCHAR(100);
 ALTER TABLE cliente ADD COLUMN complemento VARCHAR(100);
 ALTER TABLE cliente ADD COLUMN cidade_estado VARCHAR(100);
+ALTER TABLE cliente ADD COLUMN endereco VARCHAR(100);
 
-select * from agendamento
-	
+
+select * from cliente
+
+SELECT column_name
+FROM information_schema.columns
+WHERE table_name = 'cliente';
 
 CREATE TABLE servicos (
     id_servico SERIAL PRIMARY KEY,
@@ -54,6 +59,9 @@ ALTER TABLE agendamento
 ADD COLUMN IF NOT EXISTS id_prestador INTEGER;
 
 ALTER TABLE agendamento
+ADD COLUMN status_solicitacao VARCHAR(30) DEFAULT 'pendente';
+
+ALTER TABLE agendamento
 ADD CONSTRAINT fk_agendamento_prestador
 FOREIGN KEY (id_prestador)
 REFERENCES prestador_servico(id_prestador);
@@ -68,3 +76,37 @@ INSERT INTO servicos (nome_servico, descricao_servico) VALUES
 ('Verificação de Nível de Fluídos', 'Checagem e reposição de fluídos essenciais (freio, radiador, etc.).'),
 ('Reparo de Pneus', 'Conserto de furos e pequenos danos nos pneus.'),
 ('Lavagem a Seco', 'Limpeza externa e interna do veículo sem uso excessivo de água.');
+
+SELECT * FROM prestador_servico;
+SELECT * FROM agendamento;
+SELECT * FROM cliente;
+SELECT id_prestador, nome, email
+FROM prestador_servico
+ORDER BY id_prestador;
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+ORDER BY table_name;
+
+SELECT
+    table_name,
+    column_name,
+    data_type
+FROM information_schema.columns
+WHERE table_schema = 'public'
+ORDER BY table_name, ordinal_position;
+
+
+SELECT
+    tc.table_name,
+    kcu.column_name,
+    ccu.table_name AS foreign_table_name,
+    ccu.column_name AS foreign_column_name
+FROM information_schema.table_constraints AS tc
+JOIN information_schema.key_column_usage AS kcu
+    ON tc.constraint_name = kcu.constraint_name
+JOIN information_schema.constraint_column_usage AS ccu
+    ON ccu.constraint_name = tc.constraint_name
+WHERE tc.constraint_type = 'FOREIGN KEY';
+
